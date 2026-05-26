f0f6c3d929edb5a0e997f340a98fa9a8a956d4cd
Miry075 <37035161+Miry075@users.noreply.github.com>
2026-05-26 17:42:08 +0200

    Remove unused ComputeMarginalCost method
    Removed commented-out ComputeMarginalCost method.

--- Gem.Powerplant.Domain/Models/PowerplantModel.cs
+++ Gem.Powerplant.Domain/Models/PowerplantModel.cs
@@ -9,18 +9,4 @@ public class PowerplantModel
     public double Efficiency { get; set; }
     public double PMin { get; set; }
     public double PMax { get; set; }
-
-    // public double ComputeMarginalCost(FuelsInfo fuelsInfo)
-    // {
-    //     ArgumentNullException.ThrowIfNull(fuelsInfo);
-
-    //     return Type switch
-    //     {
-    //         PowerplantType.GasFired => fuelsInfo.Gas / Efficiency,
-    //         PowerplantType.TurboJet => fuelsInfo.Kerosine / Efficiency,
-    //         PowerplantType.WindTurbine => 0,
-    //         _ => throw new ArgumentException($"The given fuel type is unknown : {Type}")
-    //     };
-    // }
-
 }
